02704a967272e0487b6e3996e255c3ade9b65f79
JohM3i <johannes.messmer@gmx,de>
2021-01-17 18:23:25 +0100

    fix of test

--- tests_rfid/tests_rfid.ino
+++ tests_rfid/tests_rfid.ino
@@ -18,11 +18,11 @@ void loop() {
 
     Rfid_Tag tag;
 
-    if(rgid_get(&tag) == RFID_OK) {
+    if(rfid_get(&tag) == RFID_OK) {
       Serial.println(tag);
       enable_buzzer(200);
     } else {
-      Serial.println("Reading rfid tag failed.")  
+      Serial.println("Reading rfid tag failed.");
     }
     
     rfid_start();
